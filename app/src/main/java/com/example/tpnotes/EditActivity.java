9f61b168e91ec41174bf61db9a3d446399e453b4
Roman Nemilov <nemilov220@gmail.com>
2024-03-06 16:22:34 +0300

    version 1.0

--- app/src/main/java/com/example/tpnotes/EditActivity.java
+++ app/src/main/java/com/example/tpnotes/EditActivity.java
@@ -1,13 +1,16 @@
 package com.example.tpnotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
 import org.w3c.dom.Text;
@@ -56,11 +59,37 @@ public class EditActivity extends AppCompatActivity {
         edBody.addTextChangedListener(textWatcher);
     }
 
+    public void onBack(View view){
+        finish();
+    }
+
+    public void onDelete(View view){
+        if (edTitle.getText().toString().trim().isEmpty() &&
+                edBody.getText().toString().trim().isEmpty()){
+            deleteAndExit();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        builder.setMessage(R.string.confirm_delete_message)
+                .setTitle(R.string.confirm_delete_title)
+                .setPositiveButton(R.string.confirm, (dialog, id) -> {
+
+                })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> {
+                    // CANCEL
+                }).show();
+    }
+
     public void save(){
         values.put(DbHelper.NOTES_ID, note.getId());
         values.put(DbHelper.NOTES_TITLE, edTitle.getText().toString());
         values.put(DbHelper.NOTES_BODY, edBody.getText().toString());
         db.update(DbHelper.TABLE_NOTES, values, DbHelper.NOTES_ID + "=?" ,
                 new String[]{String.valueOf(note.getId())});
+    }
+    public void deleteAndExit(){
+        db.delete(DbHelper.TABLE_NOTES,DbHelper.NOTES_ID + "=?" ,
+                new String[]{String.valueOf(note.getId())});
+        finish();
     }
 }
